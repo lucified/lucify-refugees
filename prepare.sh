e98a04a077b3e596f4a862e62e7c0433673cdd82
Juho Ojala <juhopekkaolavi@gmail.com>
2015-09-13 22:58:35 +0300

    More countries needed for HD

--- prepare.sh
+++ prepare.sh
@@ -4,7 +4,7 @@ rm -f data/map.json
 
 ogr2ogr \
   -f GeoJSON \
-  -where "ADM0_A3 IN ('SYR', 'AFG', 'SRB', 'IRQ', 'ALB', 'ERI', 'PAK', 'SOM', 'CHI', 'UKR', 'TUR', 'CYP', 'PSE') OR Continent IN ('Europe') OR REGION_WB IN ('Sub-Saharan Africa', 'Middle East & North Africa') OR SUBREGION IN ('Western Asia', 'Central Asia', 'Southern Asia', 'Eastern Asia')" \
+  -where "ADM0_A3 IN ('SYR', 'AFG', 'SRB', 'IRQ', 'ALB', 'ERI', 'PAK', 'SOM', 'CHI', 'UKR', 'TUR', 'CYP', 'PSE') OR Continent IN ('Europe') OR REGION_WB IN ('Sub-Saharan Africa', 'Middle East & North Africa') OR SUBREGION IN ('Western Asia', 'Central Asia', 'Southern Asia', 'Eastern Asia', 'South-Eastern Asia')" \
   data/map.json \
   data/ne_10m_admin_0_countries.shp
 
