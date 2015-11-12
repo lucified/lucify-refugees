d212dcb90394dcdd9a634053ec8788d278dc332f
Ville Saarinen <ville.saarinen@gmail.com>
2015-11-12 10:07:17 +0200

    Updated UNHCR asylum data to Nov 11 version
    Includes September data for Finland, and October data for some countries

--- src/js/model/refugee-constants.js
+++ src/js/model/refugee-constants.js
@@ -13,7 +13,7 @@ module.exports.DATA_END_MOMENT = moment([
 	module.exports.DATA_END_YEAR,
 	module.exports.DATA_END_MONTH, 30]);
 
-module.exports.ASYLUM_APPLICANTS_DATA_UPDATED_MOMENT = moment([2015, 10, 5]);
+module.exports.ASYLUM_APPLICANTS_DATA_UPDATED_MOMENT = moment([2015, 10, 11]);
 module.exports.SYRIA_REFUGEES_DATA_UPDATED_MOMENT = moment([2015, 10, 3]);
 
 module.exports.disableLabels = ['BIH', 'MKD', 'ALB', 'LUX', 'MNE', 'ARM', 'AZE', 'LBN'];
