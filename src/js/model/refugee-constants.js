f4be576c4a3729fe0212cd67e679e07b43f40a95
Ville Saarinen <ville.saarinen@gmail.com>
2016-02-07 15:29:04 +0200

    Show visualisation until the end of 2015

--- src/js/model/refugee-constants.js
+++ src/js/model/refugee-constants.js
@@ -7,11 +7,11 @@ module.exports.DATA_START_YEAR = 2012;
 module.exports.DATA_START_MONTH = 0;
 
 module.exports.DATA_END_YEAR = 2015;
-module.exports.DATA_END_MONTH = 10;
+module.exports.DATA_END_MONTH = 11;
 
 module.exports.DATA_END_MOMENT = moment([
-	module.exports.DATA_END_YEAR,
-	module.exports.DATA_END_MONTH]).endOf('month');
+  module.exports.DATA_END_YEAR,
+  module.exports.DATA_END_MONTH]).endOf('month');
 
 module.exports.ASYLUM_APPLICANTS_DATA_UPDATED_MOMENT = moment([2016, 1, 5]);
 module.exports.SYRIA_REFUGEES_DATA_UPDATED_MOMENT = moment([2016, 1, 7]);
