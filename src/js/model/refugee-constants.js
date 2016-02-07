d0aad2ccab5bd49f003eca7e8c5c3a24e5a07bda
Ville Saarinen <ville.saarinen@gmail.com>
2016-02-07 15:25:32 +0200

    Update asylum seekers data to Feb. 5th version
    Adds October data for UK, November data for Austria, Denmark, Spain, France, UK, Greece and Italy, and Decemer data for many countries

--- src/js/model/refugee-constants.js
+++ src/js/model/refugee-constants.js
@@ -13,7 +13,7 @@ module.exports.DATA_END_MOMENT = moment([
 	module.exports.DATA_END_YEAR,
 	module.exports.DATA_END_MONTH]).endOf('month');
 
-module.exports.ASYLUM_APPLICANTS_DATA_UPDATED_MOMENT = moment([2016, 0, 6]);
+module.exports.ASYLUM_APPLICANTS_DATA_UPDATED_MOMENT = moment([2016, 1, 5]);
 module.exports.SYRIA_REFUGEES_DATA_UPDATED_MOMENT = moment([2016, 0, 6]);
 
 module.exports.disableLabels = ['BIH', 'MKD', 'ALB', 'LUX', 'MNE', 'ARM', 'AZE', 'LBN'];
