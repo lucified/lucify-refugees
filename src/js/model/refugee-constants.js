fdf5426d1dc0ff1c24c8ddfc60e4e94217f6e716
Ville Saarinen <ville.saarinen@gmail.com>
2016-03-31 13:45:12 +0300

    Update asylum seekers data to 29.3.2016 version

--- src/js/model/refugee-constants.js
+++ src/js/model/refugee-constants.js
@@ -6,15 +6,14 @@ var moment = require('moment');
 module.exports.DATA_START_YEAR = 2012;
 module.exports.DATA_START_MONTH = 0;
 
-module.exports.DATA_END_YEAR = 2015;
-module.exports.DATA_END_MONTH = 11;
+module.exports.DATA_END_YEAR = 2016;
+module.exports.DATA_END_MONTH = 0;
 
 module.exports.DATA_END_MOMENT = moment([
   module.exports.DATA_END_YEAR,
   module.exports.DATA_END_MONTH]).endOf('month');
 
-module.exports.ASYLUM_APPLICANTS_DATA_UPDATED_MOMENT = moment([2016, 1, 5]);
-module.exports.SYRIA_REFUGEES_DATA_UPDATED_MOMENT = moment([2016, 1, 7]);
+module.exports.ASYLUM_APPLICANTS_DATA_UPDATED_MOMENT = moment([2016, 2, 29]);
 
 module.exports.disableLabels = ['BIH', 'MKD', 'ALB', 'LUX', 'MNE', 'ARM', 'AZE', 'LBN'];
 
