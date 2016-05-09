e51106b0aee34412375a7e9c4602a4467ce83bf3
Ville Saarinen <ville.saarinen@gmail.com>
2016-05-09 22:04:34 +0300

    Extend visualisation to March 2016

--- src/js/model/refugee-constants.js
+++ src/js/model/refugee-constants.js
@@ -7,7 +7,7 @@ module.exports.DATA_START_YEAR = 2012;
 module.exports.DATA_START_MONTH = 0;
 
 module.exports.DATA_END_YEAR = 2016;
-module.exports.DATA_END_MONTH = 1;
+module.exports.DATA_END_MONTH = 2;
 
 module.exports.DATA_END_MOMENT = moment([
   module.exports.DATA_END_YEAR,
