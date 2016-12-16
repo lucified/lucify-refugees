445b8640673aa2ab5636b45541da6c24e04cb27f
Ville Saarinen <ville.saarinen@gmail.com>
2016-12-16 11:17:25 +0200

    Update data to 15.12.2016 version

--- src/js/model/refugee-constants.js
+++ src/js/model/refugee-constants.js
@@ -7,13 +7,13 @@ module.exports.DATA_START_YEAR = 2012;
 module.exports.DATA_START_MONTH = 0;
 
 module.exports.DATA_END_YEAR = 2016;
-module.exports.DATA_END_MONTH = 8;
+module.exports.DATA_END_MONTH = 9;
 
 module.exports.DATA_END_MOMENT = moment([
   module.exports.DATA_END_YEAR,
   module.exports.DATA_END_MONTH]).endOf('month');
 
-module.exports.ASYLUM_APPLICANTS_DATA_UPDATED_MOMENT = moment([2016, 10, 14]);
+module.exports.ASYLUM_APPLICANTS_DATA_UPDATED_MOMENT = moment([2016, 11, 15]);
 
 module.exports.disableLabels = ['BIH', 'MKD', 'ALB', 'LUX', 'MNE', 'ARM', 'AZE', 'LBN'];
 
