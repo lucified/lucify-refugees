ab5e4c1509982669beef46133eacec69d36ad0cc
Ville Saarinen <ville.saarinen@gmail.com>
2017-04-06 09:45:25 +0300

    Fix deployment path

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var opts = {
   paths: ['node_modules/lucify-commons'],
   publishFromFolder: 'dist',
-  assetContext: 'embed/the-flow-towards-europe-updating/'
+  assetContext: process.env.MINARD ? '/' : 'embed/the-flow-towards-europe-updating/'
 };
 
 var builder = require('lucify-component-builder');
