61e93434174cf3a203a2367a2116d426c2cee5a1
Ville Saarinen <ville.saarinen@gmail.com>
2018-01-24 09:43:27 +0200

    Fix page title

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var gulp = require('gulp');
 var opts = {
   paths: ['node_modules/lucify-commons'],
   publishFromFolder: 'dist',
+  pageDef: {
+    title: 'The Flow Towards Europe - Lucify'
+  },
   assetContext: process.env.MINARD ? '' : 'embed/the-flow-towards-europe-updating/'
 };
 
